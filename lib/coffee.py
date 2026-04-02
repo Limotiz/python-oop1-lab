791d77ea7688a6c28b12d87b926f78d56b42de41
david musembi <dmusembi702@gmail.com>
2026-04-03 01:59:02 +0300

    Initial commit

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+        
+    def set_size(self, value):
+        if value in ["Small, Medium, or Large"]:
+            self.size = value
+        else:
+            print("size must be Small, Medium, or Large")
+            self.size = "Small"    
+
+    def tip(self):   
+        print("The kind of coffee which makes me come back! ")
+        self.price += 10 
+
+        

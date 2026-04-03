e4e3d1e46f893b3c390e0e642ef44e433446b333
david musembi <dmusembi702@gmail.com>
2026-04-04 01:45:23 +0300

    updated book and cofee

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python3
-
 class Coffee:
     def __init__(self, size, price):
         self.size = size
         self.price = price
-        
-    def set_size(self, value):
-        if value in ["Small, Medium, or Large"]:
-            self.size = value
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if value in ["Small", "Medium", "Large"]:
+            self._size = value
         else:
             print("size must be Small, Medium, or Large")
-            self.size = "Small"    
 
-    def tip(self):   
-        print("The kind of coffee which makes me come back! ")
-        self.price += 10 
-
-        
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
